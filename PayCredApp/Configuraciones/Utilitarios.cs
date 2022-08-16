48419756cfa2167c5e1f1b02f0b3b433f0b36491
LuisDavidSanchezOvalles <luis_sanchez2@ucne.edu.do>
2022-08-16 18:22:05 -0400

    Programando Enviar Correos

--- PayCredApp/Configuraciones/Utilitarios.cs
+++ PayCredApp/Configuraciones/Utilitarios.cs
@@ -1,10 +1,44 @@
-﻿namespace PayCredApp.Configuraciones
+﻿using System.Text.RegularExpressions;
+
+namespace PayCredApp.Configuraciones
 {
-	public class Utilitarios
+	public static class Utilitarios
 	{
 		public static decimal ConverToN2(decimal valor)
 		{
 			return Math.Round(valor,2);
 		}
-	}
+
+        public static string ToStringFromArray(this object[] array, string separator = ", ")
+        {
+            string content = "";
+            foreach (var item in array)
+            {
+                content += content == "" ? item.ToString() : separator + item.ToString();
+            }
+            return content;
+        }
+
+        public static bool Contains(this string text, string[] keywods, bool ignoreCase = false)
+        {
+            bool found = false;
+            text = ignoreCase ? text.ToLower() : text;
+
+            foreach (var keyword in keywods)
+            {
+                var keywordTemp = ignoreCase ? keyword.ToLower() : keyword;
+                if (text.Contains(keywordTemp.Trim()))
+                {
+                    found = true;
+                    break;
+                }
+            }
+            return found;
+        }
+
+        public static bool IsValidEmail(this string email)
+        {
+            return Regex.IsMatch(email ?? "", @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+        }
+    }
 }
